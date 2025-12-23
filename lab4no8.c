61b81ab20af7684bf7540b6346e7a3730a20e764
natthaphong <s6704062611336@email.kmutnb.ac.th>
2025-12-23 18:30:44 +0700

    Add files via upload

---
+++ lab4no8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    
+    while (1) {
+        int score;
+        scanf("%d", &score);
+
+        if (score == -1) {
+            break;
+        } else {
+            if (score >= 68) {
+                if (score >= 70){
+                    printf("%d(A)\n",score);
+                } else {
+                    if (score >= 80) {
+                        printf("%d(B)\n",score);
+                    } else {
+                        printf("%d(C)\n",score);
+                    }
+                }
+            } else {
+                if (score >= 60) {
+                    printf("%d(D)\n",score);
+                } else {
+                    printf("%d(F)\n",score);
+                }
+            }
+        }
+    }
+   
+}
