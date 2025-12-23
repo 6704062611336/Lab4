89ea99f592d97c0954af9e35b4c2089b50f26748
natthaphong <s6704062611336@email.kmutnb.ac.th>
2025-12-23 20:08:44 +0700

    Add files via upload

--- lab4no9.c
+++ lab4no9.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int a=0,b=0,c=0,d=0,f=0;
-
     while (1) {
         int score;
         scanf("%d", &score);
@@ -11,25 +9,19 @@ int main() {
             break;
         }
 
-        if (score >= 80) {
+        if (score >= 87) {
             printf("%d(A)\n", score);
-            a++;
-        } else if (score >= 70) {
+        } else if (score >= 79) {
             printf("%d(B)\n", score);
-            b++;
-        } else if (score >= 68) {
+        } else if (score >= 71) {
             printf("%d(C)\n", score);
-            c++;
-        } else if (score >= 60) {
+        } else if (score >= 63) {
             printf("%d(D)\n", score);
-            d++;
         } else {
             printf("%d(F)\n", score);
-            f++;
         }
     }
 
-    printf("A(%d)\nB(%d)\nC(%d)\nD(%d)\nF(%d)\n", a,b,c,d,f);
     return 0;
 }
 
